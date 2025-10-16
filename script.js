48b3cb3eb4617d2bad39e79f4774c296b9899810
fanat0m <anrrtp@mail.ru>
2025-10-16 11:08:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ function showWelcomeMessage() {
     alert("Добро пожаловать на страницу нашей команды!");
 }
 
+
+function showSkills(){
+    alert("Навыки участника: быть самым хорошим!");
+}
+
 // Показ функции после загрузки странички
 window.onload = showWelcomeMessage;
